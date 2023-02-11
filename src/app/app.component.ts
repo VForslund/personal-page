511f9bcc94de3a87aa3cc9c49745848f784643ce
Victor Forslund <victor.forslund@csamhealth.com>
2023-02-11 17:49:26 +0100

    Basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import {DeviceDetectorService, DeviceInfo} from "ngx-device-detector";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mywebsite';
+
+  isMobile: Boolean = false;
+  constructor(private deviceService: DeviceDetectorService) {
+    this.isMobile = this.deviceService.isMobile() || this.deviceService.isTablet();
+  }
+
 }
