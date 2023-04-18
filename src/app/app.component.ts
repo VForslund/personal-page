6e62e3b20b9ec8360a5cadaca561d214b670af24
Victor Forslund <vicfor002@gmail.com>
2023-04-18 20:40:13 +0200

    Reload 404 error fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {DeviceDetectorService} from "ngx-device-detector";
 import {CommonService} from "./common.service";
 import {MatIconRegistry} from "@angular/material/icon";
 import {DomSanitizer} from "@angular/platform-browser";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +17,16 @@ export class AppComponent {
   constructor(private deviceService: DeviceDetectorService,
               private commonService: CommonService,
               private matIconRegistry: MatIconRegistry,
-              private domSanitizer: DomSanitizer
+              private domSanitizer: DomSanitizer,
+              private router: Router
   ) {
+    const path = localStorage.getItem('path');
+    if(path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+
+
     this.commonService.setIsMobile(this.deviceService.isMobile() || this.deviceService.isTablet());
     this.matIconRegistry.addSvgIcon(
       `github`,
